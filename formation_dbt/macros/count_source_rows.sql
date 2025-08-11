b54dd8fa850e9dd0f458d3cbd07906bf9ffc37ed
youla1u <m_youla@yahoo.fr>
2025-08-11 18:02:52 +0200

    Ajout d'un fichier macros pour compter le nobre de ligne avant la transformation des données

--- formation_dbt/macros/count_source_rows.sql
+++ formation_dbt/macros/count_source_rows.sql
@@ -1,3 +1,16 @@
+/*
+  Macro dbt : count_source_rows
+  -----------------------------
+  Cette macro exécute une requête SQL pour compter le nombre total de lignes
+  présentes dans la table source brute 'row_yellow_tripdata' avant toute
+  transformation. Le résultat est affiché dans les logs dbt sous la forme :
+  "Nombre de lignes avant transformation : X".
+
+  Usage : 
+  Dans le terminal, lancez `dbt run-operation count_source_rows` pour voir
+  ce nombre directement dans la console, sans créer de table ou vue supplémentaire.
+*/
+
 {% macro count_source_rows() %}
     {% set query %}
         SELECT COUNT(*) AS rows_before_transformation
